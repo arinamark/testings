195cf07774cba24cccf61d1fd74c353c2e2c2916
marinarolla <averyanova.m@gmail.com>
2021-09-23 19:47:45 +0300

    lesson about assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,8 +1,8 @@
 from capitalize import capitalize
 
 
-assert capitalize("hello") != "Hello"
+assert capitalize("hello") == "Hello"
 
-assert capitalize("") != ""
+assert capitalize("") == ""
 
 print("Все тесты пройдены!")
